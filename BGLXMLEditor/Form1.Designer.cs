1f0dcf58c8acd9ba03b40f026e45689800e7cc2d
Jean-Ivonique Lehnberg <Ivo.Lehnberg@gmx.de>
2020-11-16 09:18:11 +0100

    Änderungen

--- BGLXMLEditor/Form1.Designer.cs
+++ BGLXMLEditor/Form1.Designer.cs
@@ -243,7 +243,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BGLXML-Editor";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
--- BGLXMLEditor/Form1.cs
+++ BGLXMLEditor/Form1.cs
@@ -374,10 +374,6 @@ namespace BGLXMLEditor
                 return false;
             }
         }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
         //------------------------------------------------------------------------------------//
     }
 }
